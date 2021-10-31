f786e66eb6639a8b8717b14aaea8c561448aaed9
Milos Subak <subakmil@gmail.com>
2021-10-31 15:01:24 +0100

    views and urls basics

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse('Here are our products')
+
+
+def project(request, pk):
+    return HttpResponse('Single project' + " " + str(pk))
